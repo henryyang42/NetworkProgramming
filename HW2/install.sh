5dd19a2175a98c662ff48496ccf7ed716c4151ec
henryyang42 <henryyang42@gmail.com>
2015-05-17 04:59:06 +0800

    test packet loss PK

--- HW2/install.sh
+++ HW2/install.sh
@@ -4,3 +4,5 @@ sudo apt-get install g++
 sudo apt-get install libsqlite3-dev
 sudo apt-get install sqlite3
 sudo apt-get install libunac1-dev
+#sudo  tc qdisc add dev eth0 root netem loss 90%
+#sudo tc qdisc del dev eth0 root
