f46dcc2a352789480dc76edcefe9da6e6c9f505d
henryyang42 <henryyang42@gmail.com>
2015-06-04 01:06:56 +0800

    finish parallel download

--- HW3/HW3_101062142_Cli.cpp
+++ HW3/HW3_101062142_Cli.cpp
@@ -10,12 +10,15 @@ struct sockaddr_in servaddr;
 string input, state = "greet", cmd, username, article, id, reply, filename, serv_ip;
 vector<string> tok;
 map<string, pair<string, int> >ip_port;
-struct SF{
+map<string, vector<string> >file_archive;
+map<string, vector<string> >::iterator fa_it;
+struct SF {
     int part;
+    int tot_part;
     string filename;
     string ip;
     int port;
-}sf_get, sf_send;
+} sf_get, sf_send;
 
 void *send_file(void *ptr) {
     log("SEND FILE");
@@ -23,45 +26,59 @@ void *send_file(void *ptr) {
     struct stat filestat;
     char sendline[MAXLINE] = {};
     SF sf = *((SF *) ptr);
-    FILE *fp = fopen(("Upload/" + sf.filename).c_str(), "rb");
+    FILE *fp;
+    fp = fopen(("Upload/" + sf.filename).c_str(), "rb");
     lstat(("Upload/" + sf.filename).c_str(), &filestat);
-    int file_sz = filestat.st_size, yes = 1;
+    long long file_sz = filestat.st_size, offset = 0;
     sf.port += sf.part + 1;
     int listenfd = listen2fd(addr, sf.port);
     int addr_len = sizeof(client_addr);
     int connfd = accept(listenfd, (SA *)&client_addr, (socklen_t *)&addr_len);
-    sprintf(sendline, "%d", file_sz);
+    char *file = (char*)malloc(sizeof(char) * file_sz);
+    fread(file, sizeof(char), file_sz, fp);
+    fclose(fp);
+    if (sf.tot_part) {
+        offset = (file_sz * sf.part) / sf.tot_part;
+        file_sz = (file_sz * (sf.part + 1)) / sf.tot_part - offset;
+    }
+    sprintf(sendline, "%lld", file_sz);
     log(sendline);
     write(connfd, sendline, strlen(sendline));
     read(connfd, sendline, MAXLINE);
 
-    while (!feof(fp)) {
-        int numbytes = fread(sendline, sizeof(char), sizeof(sendline), fp);
+    while (file_sz > 0) {
+        int numbytes = min((long long)MAXLINE, file_sz);
+        memcpy(sendline, file + offset, numbytes);
+        offset += numbytes;
+        file_sz -= numbytes;
+        printf("%lld\n", file_sz);
         numbytes = write(connfd, sendline, numbytes);
         memset(sendline, 0, sizeof(sendline));
         usleep(1);
     }
 
     log("END SEND FILE");
-    fclose(fp);
     close(connfd);
     close(listenfd);
+    free(file);
     fflush(stdout);
+    return NULL;
 }
 
 void *get_file(void *ptr) {
     log("GET FILE");
-    usleep(500000);
+    usleep(50000);
     char recvline[MAXLINE] = {};
     struct sockaddr_in addr;
     SF sf = *((SF *) ptr);
-    FILE *fp = fopen(("Download/" + sf.filename).c_str(), "wb");
+    FILE *fp;
+    fp = fopen(("Download/" + sf.filename).c_str(), "wb");
     sf.port += sf.part + 1;
     int connfd = connect2fd(addr, sf.ip.c_str(), sf.port), yes = 1;
     read(connfd, recvline, MAXLINE);
     write(connfd, recvline, strlen(recvline));
     int file_sz = atoi(recvline);
-    while (file_sz > 0){
+    while (file_sz > 0) {
         int numbytes = read(connfd, recvline, sizeof(recvline));
         fwrite(recvline, sizeof(char), numbytes, fp);
         memset(recvline, 0, sizeof(recvline));
@@ -72,6 +89,7 @@ void *get_file(void *ptr) {
     fclose(fp);
     close(connfd);
     fflush(stdout);
+    return NULL;
 }
 
 void ip_port_send(string ip, int port, string msg) {
@@ -103,7 +121,7 @@ void panel() {
     printf("*************Hello %s*****************\n", username.c_str());
     puts("[SU]Show User [SA]Show Article [A]dd Article");
     puts("[Y]ell [T]ell [LO]gout [D]elete Account");
-    puts("[DI]ctionary");
+    puts("[DI]ctionary [U]pload [D]ownload [PD]Parallel Download");
 }
 
 void show_article_list(string input) {
@@ -172,16 +190,58 @@ void service(string input) {
     }
     char acc[MAXLINE], pwd[MAXLINE], title[MAXLINE], content[MAXLINE];
     // Client -> Server
-    if (tok[0] == "U") {
+    if (tok[0] == "PD") {
+        pthread_t t1[10];
+        string fn = tok[1];
+        SF sf_get[10];
+        int SS = 10;
+        vector<string>ns;
+        for(int i = 0; i < file_archive[fn].size(); i++){
+            if(file_archive[fn][i] != username)
+                ns.push_back(file_archive[fn][i]);
+        }
+        if (ns.size() == 0) {
+            puts("No one has that file!");
+            return;
+        }
+        for (int i = 0; i < SS; i++) {
+            string name= ns[i%ns.size()];
+            cmd = strfmt("S_D %s %s %d %d", username.c_str(), fn.c_str(), i, SS);
+            cout << cmd << endl;
+            ip_port_send(ip_port[name].first, ip_port[name].second, cmd);
+            printf("Download %s %d/%d from %s\n", fn.c_str(), i+1, SS, name.c_str());
+            sf_get[i].filename = strfmt("%s.%d", fn.c_str(), i);
+            sf_get[i].tot_part = SS;
+            sf_get[i].part = i;
+            sf_get[i].ip = ip_port[name].first;
+            sf_get[i].port = 1234;
+            pthread_create(&t1[i], NULL, get_file, (void*)&sf_get[i]);
+        }
+        cmd = "";
+        FILE *fp = fopen(("Download/" + fn).c_str(), "wb");
+        char buf[MAXLINE];
+        for (int i = 0; i < SS; i++) {
+            string filename = "Download/" + strfmt("%s.%d", fn.c_str(), i);
+            pthread_join(t1[i], NULL);
+            FILE *ff = fopen(filename.c_str(), "rb");
+            while (!feof(ff)) {
+                int numbytes = fread(buf, sizeof(char), sizeof(buf), ff);
+                fwrite(buf, sizeof(char), numbytes, fp);
+            }
+            fclose(ff);
+            exec("rm " + filename);
+        }
+        fclose(fp);
+    } else if (tok[0] == "U") {
         cmd = "D " + username + " " + tok[2];
         if (ip_port.find(tok[1]) != ip_port.end()) {
-            ip_port_send(ip_port[tok[1]].first,ip_port[tok[1]].second, cmd);
+            ip_port_send(ip_port[tok[1]].first, ip_port[tok[1]].second, cmd);
         }
         cmd = "";
     } else if (tok[0] == "D") {
-        cmd = "S_D " + username + " " +tok[2];
+        cmd = "S_D " + username + " " + tok[2];
         if (ip_port.find(tok[1]) != ip_port.end()) {
-            ip_port_send(ip_port[tok[1]].first,ip_port[tok[1]].second, cmd);
+            ip_port_send(ip_port[tok[1]].first, ip_port[tok[1]].second, cmd);
             sf_get.ip = ip_port[tok[1]].first;
             sf_get.port = 1234;
             sf_get.filename = tok[2];
@@ -189,21 +249,29 @@ void service(string input) {
             pthread_create(&t1, NULL, get_file, (void*)&sf_get);
         }
         cmd = "";
-    } else if(tok[0] == "S_D"){
+    } else if (tok[0] == "S_D") {
         if (ip_port.find(tok[1]) != ip_port.end()) {
+            SF sf_send;
             sf_send.ip = ip_port[tok[1]].first;
             sf_send.port = 1234;
+            sf_send.part = sf_send.tot_part = 0;
+            if(tok.size() >= 5){
+                sf_send.part = atoi(tok[3].c_str());
+                sf_send.tot_part = atoi(tok[4].c_str());
+            }
             sf_send.filename = tok[2];
             pthread_t t1;
             pthread_create(&t1, NULL, send_file, (void*)&sf_send);
+        } else {
+            log("name GG");
         }
-    }else if (tok[0] == "SF") {
+    } else if (tok[0] == "SF") {
         cmd = "SF";
     } else if (tok[0] == "T") {
         article = get_article(2, input);
         cmd = strfmt("S_T %s %s", username.c_str(), article.c_str());
         if (ip_port.find(tok[1]) != ip_port.end()) {
-            ip_port_send(ip_port[tok[1]].first,ip_port[tok[1]].second, cmd);
+            ip_port_send(ip_port[tok[1]].first, ip_port[tok[1]].second, cmd);
         }
         cmd = "";
     } else if (tok[0] == "L") {
@@ -248,7 +316,15 @@ void service(string input) {
     }
     // Server -> Client
     else if (tok[0] == "S_SF") {
-        cout << input << endl;
+        stringstream ss(input);
+        getline(ss, input);
+        file_archive.clear();
+        while(getline(ss, input)){
+            cout << input << endl;
+            tok = strtok(input);
+            for(int i = 1; i < tok.size(); i++)
+                file_archive[tok[0]].push_back(tok[i]);
+        }
     }
     else if (tok[0] == "S_L") {
         if (tok[1] == "SUCCESS") {
@@ -286,21 +362,41 @@ void service(string input) {
         log(("Wrong command: " + input).c_str());
         return;
     }
-    if (cmd != ""){
+    if (cmd != "") {
         send_to_server(sockfd, servaddr, cmd);
         cmd = "";
     }
 }
 void test() {
     log("TEST");
-    pthread_t t1, t2;
-    string fn;cin >> fn;
-    sf_send.filename = sf_get.filename = fn;
-    sf_send.ip = sf_get.ip = "127.0.0.1";
-    sf_send.port = sf_get.port = serv_port+1;
-    pthread_create(&t2, NULL, send_file, (void*)&sf_send);
-    sleep(1);
-    pthread_create(&t1, NULL, get_file, (void*)&sf_get);
+    pthread_t t1[10], t2[10];
+    string fn; cin >> fn;
+    int SS = 10;
+    for (int i = 0; i < SS; i++) {
+        sf_get.filename = strfmt("%s.%d", fn.c_str(), i);
+        sf_send.filename = fn;
+        sf_send.ip = sf_get.ip = "127.0.0.1";
+        sf_send.port = sf_get.port = 1234;
+        sf_send.tot_part = SS;
+        sf_get.part = sf_send.part = i;
+        pthread_create(&t2[i], NULL, send_file, (void*)&sf_send);
+        pthread_create(&t1[i], NULL, get_file, (void*)&sf_get);
+        usleep(100000);
+    }
+    FILE *fp = fopen(("Download/" + fn).c_str(), "wb");
+    char buf[MAXLINE];
+    for (int i = 0; i < SS; i++) {
+        string filename = "Download/" + strfmt("%s.%d", fn.c_str(), i);
+        pthread_join(t1[i], NULL);
+        FILE *ff = fopen(filename.c_str(), "rb");
+        while (!feof(ff)) {
+            int numbytes = fread(buf, sizeof(char), sizeof(buf), ff);
+            fwrite(buf, sizeof(char), numbytes, fp);
+        }
+        fclose(ff);
+        exec("rm " + filename);
+    }
+    fclose(fp);
 }
 
 int main(int argc, char **argv) {
@@ -316,7 +412,7 @@ int main(int argc, char **argv) {
 
     FD_ZERO(&rset);
     greet();
-
+    //test();
     while (1) {
         FD_SET(filefd, &rset);
         FD_SET(sockfd, &rset);
